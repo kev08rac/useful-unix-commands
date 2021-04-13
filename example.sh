d4f6c3c4f6190823e2eb62854f1096067bf841c6
Kevin Anderson <kev13rac@gmail.com>
2021-04-12 20:30:10 -0400

    Update example.sh

--- example.sh
+++ example.sh
@@ -8,6 +8,8 @@
 #SBATCH --output=new-job-%A_%a.out
 #SBATCH --array=1-10
 
+srun -N 1 -c 8 -t 2:00:00 --pty bash # Request an interactive node with 8 cores for 2 hours
+
 # This is a very basic template script for bash on a remote server using parallelization
 
 trap '(read -p "[$BASH_SOURCE:$LINE0] $BASH_COMMAND?")' DEBUG # FOR DEBUGGING: Turn off set -u for this to work
